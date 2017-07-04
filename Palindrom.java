fde5a15dd05f8096ae29aa575d1331bcd08db1cb
jeevajeeva <vjeeva522@gmail.com>
2017-07-04 14:55:08 +0530

    Create Palindrom.java

---
+++ Palindrom.java
@@ -0,0 +1,22 @@
+# palindrom
+import java.io.*;
+import java.util.*;
+
+class PalindromeExample{  
+  public static void main(String args[]){ 
+    Scanner sc=new Scanner(System.in);  
+    int r,sum=0,temp;    
+    int n=sc.nextInt();
+    
+    temp=n;    
+    while(n>0){    
+      r=n%10;  
+      sum=(sum*10)+r;    
+      n=n/10;    
+    }    
+    if(temp==sum)    
+    System.out.println("palindrome number ");    
+    else    
+    System.out.println("not palindrome");    
+  }  
+}
